97eb6f4750909ef72c4995cc831aa18c6de9ee4f
blaise2s <blaise.schaeffer@gmail.com>
2021-03-26 23:01:46 -0400

    chore(init): initial setup and configuration

---
+++ projects/comments/src/lib/comments.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cmt-comments',
+  template: ` <p>comments works!</p> `,
+  styles: [],
+})
+export class CommentsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ projects/comments/src/lib/comments.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommentsComponent } from './comments.component';
+
+@NgModule({
+  declarations: [CommentsComponent],
+  imports: [],
+  exports: [CommentsComponent],
+})
+export class CommentsModule {}
